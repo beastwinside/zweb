8a9569647be3eb23413ea949564326b010590308
1-UP <1584824430@qq.com>
2018-11-22 14:58:00 +0800

    1
    1

--- app/containers/maincontainer.js
+++ app/containers/maincontainer.js
@@ -1,26 +1,8 @@
 import React,{Component} from 'react'
 import styles from '../styles/maincontainer.css';//导入
 import{Router,HashRouter,Match,Route,Link,hashHistory,IndexLink} from 'react-router-dom'
-import { Carousel } from 'antd';
-import { Collapse } from 'antd';
-import { Tabs } from 'antd';
-import { Button } from 'antd';
-import { Input } from 'antd';
-import { List, Card,Table} from 'antd';
-import Login from './login.js'
-import Test from './test.js'
-import Top from './top.js'
 import Mainpage from './mainpage.js'
-import Producation from './Producation.js'
-import Showcontainercp from './showcontainercp.js'
-import Footer from './footer.js'
-import Device from './device.js'
-import Partner from './partner.js'
-import Contact from './contact.js'
-import News from './news.js'
-import Newdetail from './newdetail.js'
-import Jstz from './jstz.js'
-import Txrz from './txrz.js'
+
 
 
 class Maincontainer  extends Component{
@@ -32,25 +14,10 @@ class Maincontainer  extends Component{
 			<HashRouter history={hashHistory}>
 
 			<div>
-			<Route   path="/" component={Top} />
+		
 			<Route  exact path="/" component={Mainpage} />
-			<Route    path="/producation" component={Producation} />
-			<Route   exact path="/producation/cp1" component={Showcontainercp} />
-			<Route    path="/partner" component={Partner} />
-			<Route    path="/device" component={Device} />
-			<Route    path="/contact" component={Contact} />
-			<Route    path="/news" component={News} />
-			<Route    path="/newdetail" component={Newdetail} />
-			<Route    path="/jstz" component={Jstz} />
-			<Route    path="/txrz" component={Txrz} />
+	
 
-    		
-
-
-			
-			<Route  exact path="/test" component={Test} />
-			<Route  exact path="/login" component={Login} />
-			<Route  exact path="/" component={Footer} />
 			</div>
 			</HashRouter>
 
